317151ff83a964d7af867c7db812d86fe58085a8
futchitwo <74236683+futchitwo@users.noreply.github.com>
2022-04-17 23:36:54 +0000

    Fix vercel-function

--- api/vercel-function.js
+++ api/vercel-function.js
@@ -1,5 +1,5 @@
 const atob = require('atob');
 exports.default = (req, res) => {
-  const body = decodeURIComponent(escape(atob(req.query.q)));
+  const body = decodeURIComponent(escape(atob(req.query.q.replace(' ', '+'))));
   res.setHeader('Content-Type','image/svg+xml').send(body);
 }
