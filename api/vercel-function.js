0c970d5e1d85fc7f38e0434c34c667c52901c8f9
futchitwo <74236683+futchitwo@users.noreply.github.com>
2022-04-18 05:31:44 +0000

    Vercel function: support Node14

--- api/vercel-function.js
+++ api/vercel-function.js
@@ -1,5 +1,5 @@
 const atob = require('atob');
 exports.default = (req, res) => {
-  const body = decodeURIComponent(escape(atob(req.query.q.replaceAll(' ', '+'))));
+  const body = decodeURIComponent(escape(atob(req.query.q.replace(/ /g, '+'))));
   res.setHeader('Content-Type','image/svg+xml').send(body);
 }
